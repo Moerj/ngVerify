5a2cdaa77bbb6b972404ed414323c69ee740ca00
Moer <199525807@qq.com>
2016-06-11 02:09:09 +0800

    删除多余的代码

--- js/index.js
+++ js/index.js
@@ -6,25 +6,12 @@ var m = angular.module('APP',['ngVerify','datePicker']);
 // 测试用控制器,调用公共方法的地方注入ngVerify
 m.controller('testCtrl',function ($scope, $timeout, ngVerify) {
 
-    // 这里的$scope只是控制器上的，和表单上verify指令的$scope无关
-    // console.log($scope);
-
-    // 想要拿到表单上的$scope 使用方法:
-    // var verifyScope = verify.scope('loginForm')
-
-    /* angular.element(document).ready(function() {
+    angular.element(document).ready(function() {
     	console.log('页面加载完成，自动检测表单的验证，返回一个对象：');
-    	var re = ngVerify.check('loginForm',true);
+    	var re = ngVerify.check('loginForm',false);
     	console.info(re);
-    }); */
+    });
 
-    /* $scope.check = function () {
-        console.log('你检测了表单的验证，返回给你一个对象：');
-        var re = ngVerify.check('loginForm',true);
-        console.info(re);
-    } */
-
-    // 以ngOptions方式写seclet
     $scope.colors = [
       {name:'black'},
       {name:'white'},
